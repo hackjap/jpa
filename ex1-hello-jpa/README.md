89bc01c6af78bfb75eb6ab27256da89204509d0f
hackjap <hackjap1015@gmail.com>
2021-07-26 16:26:39 +0900

    :tada:JPA Basic

---
+++ ex1-hello-jpa/README.md
@@ -0,0 +1,2 @@
+JPA 정리
+https://www.notion.so/jsp98/JPA-76e8dd73f28d46f19943caec1ac39a4a
---
+++ ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -0,0 +1,36 @@
+package hellojpa;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+import java.util.List;
+
+public class JpaMain {
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+        EntityManager em = emf.createEntityManager();
+
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+        try{
+
+
+            // 영속
+            Member findMember1 = em.find(Member.class, 101L);
+            Member findMember2 = em.find(Member.class, 101L);
+
+            System .out.println("result = " + (findMember1 == findMember2 ));
+
+
+            tx.commit();
+        }catch (Exception e){
+            tx.rollback();
+        }finally {
+            em.close();
+        }
+
+        emf.close();
+    }
+}
